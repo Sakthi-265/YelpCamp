843407cf9e6baad3d41debbcfa5b53e35ae61570
sakthi <sakthimala26@gmail.com>
2024-06-17 20:35:13 +0530

    changed url

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ const helmet = require('helmet')
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-// const dbUrl = process.env.DB_URL    
+const dbUrl = process.env.DB_URL    
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect(dbUrl)
     .then(() => {
         console.log("DATABASE CONNECTED :)")
     })
